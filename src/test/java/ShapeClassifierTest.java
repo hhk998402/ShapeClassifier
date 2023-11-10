f2cb1f3b2e97dea68b23301376db3ba18fdc4dcd
Hemant H Kumar <hemant.hari.kumar@gmail.com>
2023-11-10 04:41:59 +0000

    Add a blackbox test looking for valid square

--- src/test/java/ShapeClassifierTest.java
+++ src/test/java/ShapeClassifierTest.java
@@ -1,8 +1,16 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ShapeClassifierTest {
+
+    ShapeClassifier s;
+
+    @BeforeEach
+    public void setup() {
+        s = new ShapeClassifier();
+    }
 
     /**
      * This is an example test 
@@ -10,8 +18,17 @@ public class ShapeClassifierTest {
     @DisplayName("Example Test")
     @Test
     public void example() {
-        ShapeClassifier s = new ShapeClassifier();
         String answer = s.evaluateGuess("Equilateral,Large,Yes,100,100,100");
         assertEquals("Yes", answer);
     }
+
+    @DisplayName("Test for valid square")
+    @Test
+    public void testValidSquare() {
+        String answer = s.evaluateGuess("Square,Small,No,1,1,1,1");
+        assertEquals("Yes", answer);
+
+    }
+
+
 }
